e0dc41eb687b0663d9476360f88d8e454d027d90
mads221h <32454208+mads221h@users.noreply.github.com>
2017-10-03 12:37:20 +0200

    Add files via upload

---
+++ spilny/spil/spil/spil/TicTacToe.cs
@@ -0,0 +1,380 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spil
+{
+    public class TicTacToe
+    {
+        public string winnerstring = "";
+        public string spillerturcheck = "to";
+        public int briktest = 0;
+        public char[,] GameBoard { get; set; }
+
+        public TicTacToe()
+        {
+            GameBoard = new char[3, 3] { {' ', ' ', ' '},
+                {' ', ' ', ' '}, 
+                { ' ', ' ', ' '} };
+        }
+
+        public string GetGameBoardView()
+        {
+            string resultat = "";
+            resultat = resultat + "Y\n";
+            resultat = resultat + "  *******************\n";
+            resultat = resultat + "  *     *     *     *\n";
+            resultat = resultat + "3 *  " + GameBoard[0, 2] + "  *  " + GameBoard[1, 2] + "  *  " + GameBoard[2, 2] + "  *\n";
+            resultat = resultat + "  *     *     *     *\n";
+            resultat = resultat + "  *******************\n";
+            resultat = resultat + "  *     *     *     *\n";
+            resultat = resultat + "2 *  " + GameBoard[0, 1] + "  *  " + GameBoard[1, 1] + "  *  " + GameBoard[2, 1] + "  *\n";
+            resultat = resultat + "  *     *     *     *\n";
+            resultat = resultat + "  *******************\n";
+            resultat = resultat + "  *     *     *     *\n";
+            resultat = resultat + "1 *  " + GameBoard[0, 0] + "  *  " + GameBoard[1, 0] + "  *  " + GameBoard[2, 0] + "  *\n";
+            resultat = resultat + "  *     *     *     *\n";
+            resultat = resultat + "  *******************\n";
+            resultat = resultat + "     1     2     3    X\n";
+
+            return resultat;
+        }
+
+        
+
+        
+
+
+
+
+        internal string Sætbrik(string v, char b)
+            
+        {
+            
+            if (v == "1")
+            {
+                if (GameBoard[0, 0] == 'x' || GameBoard[0, 0] == 'o')
+                {
+                    Console.WriteLine("Der er allerede en brik der");
+                    spillerturcheck = "forkert";
+                    Console.ReadLine();
+                }
+                else
+                {
+                    GameBoard[0, 0] = b;
+                }
+
+            }
+            if (v == "2")
+            {
+                if (GameBoard[1, 0] == 'x' || GameBoard[1, 0] == 'o')
+                {
+                    Console.WriteLine("Der er allerede en brik der");
+                    spillerturcheck = "forkert";
+                    Console.ReadLine();
+                }
+                else
+                {
+                    GameBoard[1, 0] = b;
+                }
+            }
+            if (v == "3")
+            {
+                if (GameBoard[2, 0] == 'x' || GameBoard[2, 0] == 'o')
+                {
+                    Console.WriteLine("Der er allerede en brik der");
+                    spillerturcheck = "forkert";
+                    Console.ReadLine();
+                }
+                else
+                {
+                    GameBoard[2, 0] = b;
+                }
+            }
+            if (v == "4")
+            {
+                if (GameBoard[0, 1] == 'x' || GameBoard[0, 1] == 'o')
+                {
+                    Console.WriteLine("Der er allerede en brik der");
+                    spillerturcheck = "forkert";
+                    Console.ReadLine();
+                }
+                else
+                {
+                    GameBoard[0,1] = b;
+                }
+            }
+            if (v == "5")
+            {
+                if (GameBoard[1, 1] == 'x' || GameBoard[1,1] == 'o')
+                {
+                    Console.WriteLine("Der er allerede en brik der");
+                    spillerturcheck = "forkert";
+                    Console.ReadLine();
+                }
+                else
+                {
+                    GameBoard[1, 1] = b;
+                }
+            }
+            if (v == "6")
+            {
+                if (GameBoard[2,1] == 'x' || GameBoard[2,1] == 'o')
+                {
+                    Console.WriteLine("Der er allerede en brik der");
+                    spillerturcheck = "forkert";
+                    Console.ReadLine();
+                }
+                else
+                {
+                    GameBoard[2,1] = b;
+                }
+            }
+            if (v == "7")
+            {
+                if (GameBoard[0, 2] == 'x' || GameBoard[0,2] == 'o')
+                {
+                    Console.WriteLine("Der er allerede en brik der");
+                    spillerturcheck = "forkert";
+                    Console.ReadLine();
+                }
+                else
+                {
+                    GameBoard[0,2] = b;
+                }
+            }
+            if (v == "8")
+            {
+                if (GameBoard[1,2] == 'x' || GameBoard[1,2] == 'o')
+                {
+                    Console.WriteLine("Der er allerede en brik der");
+                    spillerturcheck = "forkert";
+                    Console.ReadLine();
+                }
+                else
+                {
+                    GameBoard[1,2] = b;
+                }
+            }
+            if (v == "9")
+            {
+                if (GameBoard[2, 2] == 'x' || GameBoard[2,2] == 'o')
+                {
+                    Console.WriteLine("Der er allerede en brik der");
+                    spillerturcheck = "forkert";
+                    Console.ReadLine();
+                }
+                else
+                {
+                    GameBoard[2, 2] = b;
+                }
+            }
+            return spillerturcheck;
+            
+            }
+
+        internal void sletbrik(string v, char tur)
+        {
+            if (v == "1")
+            {
+                if (GameBoard[0, 0] == ' ')
+                {
+                    Console.WriteLine("Der er ikke en brik der");
+                    spillerturcheck = "forkert";
+                    Console.ReadLine();
+                }
+                else 
+                {
+                    GameBoard[0, 0] = ' ';
+                }
+                
+                
+
+            }
+            if (v == "2")
+            {
+                if (GameBoard[1, 0] == ' ')
+                {
+                    Console.WriteLine("Der er ikke en brik der");
+                    spillerturcheck = "forkert";
+                    Console.ReadLine();
+                }
+                else 
+                {
+                    GameBoard[1, 0] = ' ';
+                }
+                
+            }
+            if (v == "3")
+            {
+                if (GameBoard[2, 0] == ' ')
+                {
+                    Console.WriteLine("Der er ikke en brik der");
+                    spillerturcheck = "forkert";
+                    Console.ReadLine();
+                }
+                else
+                {
+                    GameBoard[2, 0] = ' ';
+                }
+                
+            }
+            if (v == "4")
+            {
+                if (GameBoard[0, 1] == ' ')
+                {
+                    Console.WriteLine("Der er ikke en brik der");
+                    spillerturcheck = "forkert";
+                    Console.ReadLine();
+                }
+                else
+                {
+                    GameBoard[0, 1] = ' ';
+                }
+                
+            }
+            if (v == "5")
+            {
+                if (GameBoard[1, 1] == ' ')
+                {
+                    Console.WriteLine("Der er ikke en brik der");
+                    spillerturcheck = "forkert";
+                    Console.ReadLine();
+                }
+                else
+                {
+                    GameBoard[1, 1] = ' ';
+                }
+                
+            }
+            if (v == "6")
+            {
+                if (GameBoard[2, 1] == ' ')
+                {
+                    Console.WriteLine("Der er ikke en brik der");
+                    spillerturcheck = "forkert";
+                    Console.ReadLine();
+                }
+                else
+                {
+                    GameBoard[2, 1] = ' ';
+                }
+                
+            }
+            if (v == "7")
+            {
+                if (GameBoard[0, 2] == ' ')
+                {
+                    Console.WriteLine("Der er ikke en brik der");
+                    spillerturcheck = "forkert";
+                    Console.ReadLine();
+                }
+                else 
+                {
+                    GameBoard[0, 2] = ' ';
+                }
+                
+            }
+            if (v == "8")
+            {
+                if (GameBoard[1, 2] == ' ')
+                {
+                    Console.WriteLine("Der er ikke en brik der");
+                    spillerturcheck = "forkert";
+                    Console.ReadLine();
+                }
+                else 
+                {
+                    GameBoard[1, 2] = ' ';
+                }
+                
+            }
+            if (v == "9")
+            {
+                if (GameBoard[2, 2] == ' ')
+                {
+                    Console.WriteLine("Der er ikke en brik der");
+                    spillerturcheck = "forkert";
+                    Console.ReadLine();
+                }
+                else
+                {
+                    GameBoard[2, 2] = ' ';
+                }
+                
+            }
+           
+
+        }
+
+        public string Validate()
+        {
+            
+            string brikker = "ox";
+            for (int i = 0; i < 3; i = i + 1)
+            {
+                
+                var columnOneChar = GameBoard[i, 0];
+                var columnTwoChar = GameBoard[i, 1];
+                var columnThreeChar = GameBoard[i, 2];
+                if (brikker.Contains(columnOneChar) && brikker.Contains(columnTwoChar) && brikker.Contains(columnThreeChar))
+                {
+                    if (columnOneChar == columnTwoChar && columnTwoChar == columnThreeChar)
+                    {
+
+                        Console.WriteLine( columnOneChar + " Wins");
+                        winnerstring = "Winner";
+                        Console.ReadLine();
+                    }
+                }
+                var rowOneChar = GameBoard[0, i];
+                var rowTwoChar = GameBoard[1, i];
+                var rowThreeChar = GameBoard[2, i];
+                if (brikker.Contains(rowOneChar) && brikker.Contains(rowTwoChar) && brikker.Contains(rowThreeChar))
+                {
+                    if (rowOneChar == rowTwoChar && rowTwoChar == rowThreeChar)
+                    {
+                        Console.WriteLine(rowOneChar + " Wins");
+                        winnerstring = "Winner";
+                        Console.ReadLine();
+
+                    }
+                    
+                }
+                var cellOneChar = GameBoard[0, 0];
+                var cellTwoChar = GameBoard[1, 1];
+                var cellThreeChar = GameBoard[2, 2];
+                if (brikker.Contains(cellOneChar) && brikker.Contains(cellTwoChar) && brikker.Contains(cellThreeChar))
+                {
+                    if (cellOneChar == cellTwoChar &&
+                    cellTwoChar == cellThreeChar)
+                    {
+                        Console.WriteLine(cellOneChar + " Wins");
+                        winnerstring = "Winner";
+                        Console.ReadLine();
+
+                    }
+                }
+                var celOneChar = GameBoard[2, 0];
+                var celTwoChar = GameBoard[1, 1];
+                var celThreeChar = GameBoard[0, 2];
+                if (brikker.Contains(celOneChar) && brikker.Contains(celTwoChar) && brikker.Contains(celThreeChar))
+                {
+                    if (celOneChar == celTwoChar &&
+                    celTwoChar == celThreeChar)
+                    {
+                        Console.WriteLine(celOneChar + " Wins");
+                        winnerstring = "Winner";
+                        Console.ReadLine();
+                    }
+                }
+                
+            }
+            return winnerstring;
+
+        }
+
+    }
+}
---
+++ spilny/spil/spil/spil/TicTacToeMenu.cs
@@ -0,0 +1,259 @@
+﻿using System;
+
+namespace spil
+{
+    public class TicTacToeMenu
+    {
+        int Gamecheck = 0;
+        int Variation = 0;
+        TicTacToe ticTacToe {get; set; }
+        public void Show()
+        {
+            bool running = true;            
+            string choice = "";
+            do
+            {
+                ShowMenu();
+                choice = GetUserChoise();
+                switch (choice)
+                {
+                    case "1": DoActionFor1(); break;
+                    case "2": DoActionFor2(); break;
+                    case "3": DoActionFor3(); break;
+                    case "0": running = false; break;
+                    default : ShowMenuSelectionErroe(); break;
+                }
+            } while (running);
+        }
+        static int spillertur = 0;
+        
+            
+        private void ShowMenu()
+        {
+            Console.Clear();
+            if (ticTacToe != null)
+            {
+                Console.WriteLine(ticTacToe.GetGameBoardView());
+            }
+            Console.WriteLine("tic tac toe");
+            Console.WriteLine();
+            Console.WriteLine("1. Oret nyt spil");
+            Console.WriteLine("2. Set en brik");
+            Console.WriteLine("3. Flyt en brik");
+            Console.WriteLine();
+            Console.WriteLine("0. exit");
+        }
+
+        private string GetUserChoise()
+        {
+            Console.WriteLine();
+            Console.Write("Indtast dit valg: ");
+            return Console.ReadLine();
+        }
+
+        private void ShowMenuSelectionErroe()
+        {
+            Console.WriteLine("Ugyldigt valg.");
+            Console.ReadLine();
+        }
+
+        private void DoActionFor1()
+        {
+            ticTacToe = new TicTacToe();
+            spillertur = 0;
+            Gamecheck = 1;
+            Variation = 0;
+            Console.WriteLine("Vælg Variation, tryk 1 for standart eller 2 for variation.");
+
+            string VariationsValg = Console.ReadLine();
+            if (VariationsValg == "1")
+            {
+                Variation = 0;
+                Console.WriteLine("Du valgte 1");
+                Console.ReadLine();
+            }
+            else if (/*Console.ReadLine() == "2"*/VariationsValg == "2")
+            {
+                Variation = 1;
+                Console.WriteLine("Du valgte 2");
+                Console.ReadLine();
+            }
+            else
+            {
+                ticTacToe = new TicTacToe();
+                Console.WriteLine("Ugyldigt valg");
+                Console.ReadLine();
+            }
+            
+
+        }       
+        private void DoActionFor2()
+        {
+            if (Variation == 0)
+            {
+                if (Gamecheck == 1)
+                {
+                    Console.WriteLine("sæt en brik");
+                    char tur = 'o';
+
+                    if (spillertur == 1 || spillertur == 3 || spillertur == 5 || spillertur == 7 || spillertur == 9)
+                    {
+                        tur = 'x';
+                    }
+                    else
+                    {
+                        tur = 'o';
+                    }
+
+                    Console.WriteLine(tur);
+                    ticTacToe.Sætbrik(Console.ReadLine(), tur);
+
+
+                    //ticTacToe.spillerturcheck = "forkert";
+                    //if (ticTacToe.spillerturcheck == "to")
+                    //{
+                        spillertur++;
+                    //}
+
+                    //else if (ticTacToe.spillerturcheck == "forkert")
+                    //{
+
+                    //    spillertur = spillertur - 1;
+                    //    //Console.WriteLine("lortet virker ikke");
+
+                    //    //ticTacToe.spillerturcheck = 0;
+                    //}
+                    ticTacToe.Validate();
+                    Console.ReadLine();
+                    if (ticTacToe.winnerstring == "Winner")
+                    {
+                        DoActionFor1();
+                    }
+                    if (spillertur >= 10)
+                    {
+                        spillertur = 0;
+                    }
+                }
+                else
+                {
+                    DoActionFor1();
+                }
+            }
+            else if (Variation == 1)
+            {
+                if (spillertur < 6)
+                {
+                    Console.WriteLine("sæt en brik");
+                    char tur = 'o';
+
+                    if (spillertur == 1 || spillertur == 3 || spillertur == 5 || spillertur == 7 || spillertur == 9)
+                    {
+                        tur = 'x';
+                    }
+                    else
+                    {
+                        tur = 'o';
+                    }
+
+                    Console.WriteLine(tur);
+                    ticTacToe.Sætbrik(Console.ReadLine(), tur);
+
+
+                    //ticTacToe.spillerturcheck = "forkert";
+                    
+                    spillertur++;
+                    ticTacToe.Validate();
+                    if (ticTacToe.winnerstring == "Winner")
+                    {
+                        DoActionFor1();
+                    }
+
+                }
+                else
+                {
+                    DoActionFor3();
+                }
+            }
+        }
+        
+        private void DoActionFor3()
+        {
+            if (Variation == 1 && spillertur >= 6)
+            {
+                if (Gamecheck == 1)
+                {
+                    ticTacToe.briktest = 0;
+                    Console.WriteLine("vælg en brik at flytte");
+                    char tur = 'o';
+
+                    if (spillertur == 1 || spillertur == 3 || spillertur == 5 || spillertur == 7 || spillertur == 9)
+                    {
+                        tur = 'x';
+                    }
+                    else
+                    {
+                        tur = 'o';
+                    }
+
+                    Console.WriteLine(tur);
+                    ticTacToe.sletbrik(Console.ReadLine(), tur);
+                    ticTacToe.Validate();
+
+                    if (ticTacToe.briktest == 0)
+                    {
+                        Console.WriteLine("sæt en brik");
+
+
+                        if (spillertur == 1 || spillertur == 3 || spillertur == 5 || spillertur == 7 || spillertur == 9)
+                        {
+                            tur = 'x';
+                        }
+                        else
+                        {
+                            tur = 'o';
+                        }
+
+                        Console.WriteLine(tur);
+                        ticTacToe.Sætbrik(Console.ReadLine(), tur);
+
+
+                        //ticTacToe.spillerturcheck = "forkert";
+
+                        spillertur++;
+                        ticTacToe.Validate();
+                        
+                        if (ticTacToe.winnerstring == "Winner")
+                        {
+                            DoActionFor1();
+                        }
+                        if (spillertur >= 10)
+                        {
+                            spillertur = 6;
+                        }
+                    }
+                    else
+                    {
+                        DoActionFor3();
+                    }
+
+
+                }
+
+                else
+                {
+                    DoActionFor1();
+                }
+            }
+            else if (spillertur < 6)
+            {
+                Console.WriteLine("Ugyldigt valg sæt 6 brikker ført");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Forkert valg, vælg variation 2 for at flytte brikker");
+                Console.ReadLine();
+            }
+        }
+    }
+}
