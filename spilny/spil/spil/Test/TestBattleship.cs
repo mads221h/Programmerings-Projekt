fa1733ed8f0efcc213bdb9c1c254b68ad7764da1
Nicolai Overby Andersen <nico706a@edu.eal.dk>
2017-10-10 23:03:43 +0200

    Version før at jeg er færdig for idag, har added endnu en test klasse som skal teste battleship delen.

---
+++ spilny/spil/spil/Test/TestBattleship.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using spil;
+
+namespace Test
+{ 
+    [TestClass]
+    public class TestBattleship
+    {
+        //[TestMethod]
+        //public void NeitherPlayerHasThreeInARow()
+        //{
+        //    TicTacToe ticTacToe = new TicTacToe();
+        //    const string expectet = "";
+        //    string actual = ticTacToe.Validate();
+        //    Assert.AreEqual(expectet, actual);
+        //}
+
+        //[TestMethod]
+        //public void AShipIsPlaced()
+        //{
+        //    Battleship battleship = new Battleship();
+
+        //    const string expectet = "";
+
+        //    string actual = ;
+
+        //    Assert.AreEqual(expectet, actual);
+        //}
+
+
+
+    }
+}
+    
+
